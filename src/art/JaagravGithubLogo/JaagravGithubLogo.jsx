98b61a8668fef53d3e5674430e8060286e596199
Jaagrav <jaagravseal03@gmail.com>
2021-03-29 14:25:18 +0530

    Added Tail

--- src/art/JaagravGithubLogo/JaagravGithubLogo.jsx
+++ src/art/JaagravGithubLogo/JaagravGithubLogo.jsx
@@ -19,10 +19,9 @@ function JaagravGithubLogo() {
           <div className={style.rightEar}></div>
         </div>
         <div className={style.body}>
-          {/* <svg className={style.tail} width="58" height="37" viewBox="0 0 58 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg className={style.tail} width="58" height="37" viewBox="0 0 58 37" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M24.1628 10.8642C30.4901 19.4505 37.0925 26.7648 58 21.0406V35.4052C56.8996 35.1932 54.6988 36.3051 47.2711 36.9411C39.8435 37.5771 30.215 32.966 24.1628 27.7188C18.1106 22.4716 17.8355 17.2244 12.3336 10.8642C6.83157 4.504 2.70507 6.73008 0.779377 5.77605C-1.14632 4.82202 0.229179 -0.584164 7.93196 0.0518579C15.6347 0.687879 17.8355 2.27793 24.1628 10.8642Z" fill="#2D333B"/>
-          </svg> */}
-          <div className={style.tail}></div>
+          </svg>
         </div>
 
         

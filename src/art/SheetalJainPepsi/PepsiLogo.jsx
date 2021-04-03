8edc25db311726f724e68fe4bae87a38b081ada0
sheetalj2205 <sheetalj2205@gmail.com>
2021-04-03 19:09:23 +0530

    circle design added

---
+++ src/art/SheetalJainPepsi/PepsiLogo.jsx
@@ -0,0 +1,31 @@
+import style from "./style.module.css";
+import { Credit } from "../../components/Credit/index";
+
+// Replace ComponentName with YourName followed by ArtName.
+// For example, SalilReactLogo or SalilNaikReactLogo
+// component name, file name and folder name should be same. 
+
+function ComponentName() {
+  // Enter your name, github url and art-name below
+  let data = {
+    name: "Sheetal Jain",
+    "gh-link": "https://github.com/sheetalj2205",
+    "art-name": "Circle Figure",
+  };
+  return (
+    <div className={`${style.container} container`}>
+      {/* DO NOT edit the line above*/}
+
+      {/* your art blocks will come here */}
+      <div className={`${style.pepsi} pepsi`}>
+
+        
+      </div>
+
+      {/* do not edit the line below */}
+      <Credit data={data} />
+    </div>
+  );
+}
+
+export default ComponentName;

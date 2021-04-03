7ee173f482b34d7ddf5d5c8b261511552fb217ad
tarunsingh7379 <tarunsinghofficial236@gmail.com>
2021-04-03 19:28:55 +0530

    Added a Git Logo

---
+++ src/art/TarunGitLogo/TarunGitLogo.jsx
@@ -0,0 +1,27 @@
+import style from "./style.module.css";
+import { Credit } from "../../components/Credit/index";
+
+function TarunGitLogo() {
+  let data = {
+    name: "Tarun Singh",
+    "gh-link": "https://github.com/tarunsingh7379",
+    "art-name": "Git Logo",
+  };
+  return (
+    <div className={`${style.container} container`}>
+      {/* DO NOT edit the className above*/}
+
+      {/* your art blocks will come here */}
+      <div className={style.center}>
+        <div className={style.logo}>
+          <div className={style.line1}></div>
+          <div className={style.line2}></div>
+        </div>
+      </div>
+      {/* do not edit the line below */}
+      <Credit data={data} />
+    </div>
+  );
+}
+
+export default TarunGitLogo;

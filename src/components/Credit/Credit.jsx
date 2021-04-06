c04d7aa7e4fdccff545c96dbe78e8e6559b10be8
Jaagrav <jaagravseal03@gmail.com>
2021-04-06 22:10:06 +0530

    Added like button

--- src/components/Credit/Credit.jsx
+++ src/components/Credit/Credit.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import style from "./style.module.css";
 
+import {MdFavoriteBorder, MdFavorite} from "react-icons/md";
+
 export function Credit(props) {
   const creditsRef = useRef();
+  const [noOfLikes, setNoOfLikes] = useState(79)
+  const [liked, setLiked] = useState(false);
+  
+  const likeArt = () => {
+    setLiked(!liked)
+  }
 
   useEffect(() => {
     creditsRef.current.parentElement.addEventListener("mouseover", (e) => {
@@ -12,15 +20,21 @@ export function Credit(props) {
     creditsRef.current.parentElement.addEventListener("mouseleave", (e) => {
       creditsRef.current.classList.remove(style.hover);
     });
+    // Hiding and showing Credits on hover
   }, []);
 
   return (
     <div className={style.credits} ref={creditsRef}>
       <div className={style.credit}>
-      {props.data["art-name"]} by{" "}
-      <a href={props.data["gh-link"]} target="_blank" rel="noreferrer">
-        {props.data.name}
-      </a>
+        {props.data["art-name"]} by{" "}
+        <a className={style.highlighted} href={props.data["gh-link"]} target="_blank" rel="noreferrer">
+          {props.data.name}
+        </a>
+      </div>
+      <div onClick={likeArt} className={`${style.likes} ${liked ? style.highlighted : ""}`}>
+        <span className={style.likesNum}>{noOfLikes}</span>
+        {liked && <MdFavorite className={style.likeIcon}/>}
+        {!liked && <MdFavoriteBorder className={style.likeIcon}/>}
       </div>
     </div>
   );

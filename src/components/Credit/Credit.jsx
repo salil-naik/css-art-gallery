be50a02a46683342a97bc01a25fad3fe5371511f
Jaagrav <jaagravseal03@gmail.com>
2021-04-06 20:42:06 +0530

    Fixed: Credits Style Rules

--- src/components/Credit/Credit.jsx
+++ src/components/Credit/Credit.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+import style from "./style.module.css";
+
 export function Credit(props) {
+  const creditsRef = useRef();
+
+  useEffect(() => {
+    creditsRef.current.parentElement.addEventListener("mouseover", (e) => {
+      creditsRef.current.classList.add(style.hover);
+    });
+    
+    creditsRef.current.parentElement.addEventListener("mouseleave", (e) => {
+      creditsRef.current.classList.remove(style.hover);
+    });
+  }, []);
+
   return (
-    <div className="credits">
+    <div className={style.credits} ref={creditsRef}>
+      <div className={style.credit}>
       {props.data["art-name"]} by{" "}
       <a href={props.data["gh-link"]} target="_blank" rel="noreferrer">
         {props.data.name}
       </a>
+      </div>
     </div>
   );
 }

664f8c76707414a5582570085b89c5dc788617c3
Jaagrav <jaagravseal03@gmail.com>
2021-04-07 03:06:47 +0530

    Removed firebase analytics to prevent credentials leak

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,7 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.analytics();
 
 const artsDB = firebase.database().ref("css-art-gallery/arts");
 
